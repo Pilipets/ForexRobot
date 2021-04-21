2f66856df0bf3c8b042d892c2788f387950f7701
Hlib Pylypets <pilipets.gleb@gmail.com>
2021-04-21 16:13:56 +0300

    Created separate project for fxcm bot
    Signed-off-by: Hlib Pylypets <pilipets.gleb@gmail.com>

--- VS2/TradingRobot/robot/fxcm_robot.py
+++
@@ -1,7 +0,0 @@
-
-class FxcmBot:
-    def __init__(self, config):
-        self.config = config
-
-        if type(config) == AlpacaConfig: self.core = AlpacaCore(config)
-        else: self.core = None
